28b90edf6f9a639c2bcd7d2be3e1f001069386c4
Lutz <derlurtz@gmail.com>
2012-05-28 16:24:53 +0200

    bla

--- src/opengl.cpp
+++ src/opengl.cpp
@@ -7,8 +7,10 @@
 #include <cstdlib>
 #include <limits>
 #include "opengl.h"
+#include "Trackball.h"
 
 MeshObj const * _meshobj;
+Trackball _ball;
 
 #define BUFFER_OFFSET(i) ((char *)NULL + (i))
 
@@ -19,19 +21,29 @@ float camX, camY, camZ = 4.0f; //X, Y, and Z
 float lastx, lasty, xrot, yrot, xrotrad, yrotrad; //Last pos and rotation
 float speed = 0.1f; //Movement speed
 
-void mouseMovement(int x, int y) {
-    int cx = windowWidth/2;
-    int cy = windowHeight/2;
-
-    if(x == cx && y == cy){ //The if cursor is in the middle
-        return;
+void mouseEvent(int button, int state, int x, int y) {
+  Trackball::MouseState mouseState;
+  if (state == GLUT_DOWN) {
+    switch (button) {
+      case GLUT_LEFT_BUTTON : {
+        mouseState = Trackball::LEFT_BTN;
+        break;
+      }
+      case GLUT_RIGHT_BUTTON : {
+        mouseState = Trackball::RIGHT_BTN;
+        break;
+      }
+      default : break;
     }
+  } else {
+    mouseState = Trackball::NO_BTN;
+  }
+  ball.updateMouseBtn(mouseState, x, y);
 
-    int diffx=x-cx; //check the difference between the current x and the last x position
-    int diffy=y-cy; //check the difference between the current y and the last y position
-    xrot += (float)diffy/2; //set the xrot to xrot with the addition of the difference in the y position
-    yrot += (float)diffx/2;// set the xrot to yrot with the addition of the difference in the x position
-    glutWarpPointer(cx, cy); //Bring the cursor to the middle
+}
+
+void mouseMoveEvent(int x, int y) {
+  ball.updateMousePos(x, y);
 }
 
 void idle() {
@@ -41,47 +53,71 @@ void idle() {
 void reshape(int width, int height) {
     windowWidth = width;
     windowHeight = height;
-    glMatrixMode(GL_PROJECTION);
-    glViewport(0, 0, width, height);
-    gluPerspective(45.0f, (GLfloat)width/(GLfloat)height, 0.5f, 10000.0f);
+    glutPostRedisplay();
+}
+
+void updateGL() {
+  GLfloat aspectRatio = (GLfloat)windowWidth / windowHeight;
+  
+  glViewport(0, 0, windowWidth, windowHeight);
+  
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(fov, aspectRatio, zNear, zFar);
+  
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  
+  ball.rotateView();
+  
+  // render //
+  renderScene();
+  
+  // swap render and screen buffer //
+  glutSwapBuffers();
 }
 
 void renderScene() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    // TODO some camera things with gluLookAt()
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-    glutSwapBuffers();
+    glTranslatef(-1, -1, -1);
+    glutSolidSphere(1.0, 10, 10);
 }
 
 void run() {
     glutMainLoop();
 }
 
-void keyboard(unsigned char key, int x, int y) {
-    switch(key) {
-        //WASD movement
-        case 'w':
-            camZ -= 0.1f;
-        break;
-        case 's':
-            camZ += 0.1f;
-        break;
-        case 'a':
-            camX -= 0.1f;
-            break;
-        case 'd':
-            camX += 0.1f;
-        break;
-        case 27:
-            //TODO some clean up
-            glutLeaveMainLoop();
-        break;
-        default:
-            std::cout << static_cast<char>(key) << std::endl;
-        }
+void keyboardEvent(unsigned char key, int x, int y) {
+  switch (key) {
+    case 'x':
+    case 27 : {
+      exit(0);
+      break;
+    }
+    case 'w': {
+      // move forward //
+      ball.updateOffset(Trackball::MOVE_FORWARD);
+      break;
+    }
+    case 's': {
+      // move backward //
+      ball.updateOffset(Trackball::MOVE_BACKWARD);
+      break;
+    }
+    case 'a': {
+      // move left //
+      ball.updateOffset(Trackball::MOVE_LEFT);
+      break;
+    }
+    case 'd': {
+      // move right //
+      ball.updateOffset(Trackball::MOVE_RIGHT);
+      break;
+    }
+    default : {
+      break;
+    }
+  }
+  glutPostRedisplay();
 }
 
 void setupOpenGL(int * argc, char ** argv, MeshObj const * const meshobj) {
@@ -95,15 +131,14 @@ void setupOpenGL(int * argc, char ** argv, MeshObj const * const meshobj) {
     glutDisplayFunc(renderScene);
     glutIdleFunc(idle);
     glutReshapeFunc(reshape);
-    glutKeyboardFunc(keyboard);
-    glutMotionFunc(mouseMovement);
+    glutKeyboardFunc(keyboardEvent);
+    glutMouseFunc(mouseEvent);
+    glutMotionFunc(mouseMoveEvent);
 
     glewInit();
     glEnable(GL_NORMALIZE);
     glEnable(GL_DEPTH_TEST);
     glShadeModel(GL_SMOOTH);
-    glEnable(GL_LIGHT0);
-    glEnable(GL_LIGHTING);
 
     _meshobj = meshobj;
 }
