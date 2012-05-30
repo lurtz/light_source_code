bb3ad559d3fdd57d923dc457b51eb9d360d85df9
Lutz <derlurtz@gmail.com>
2012-05-30 12:15:57 +0200

    fix calculation of vert_light_dir

--- shader/vertex.s
+++ shader/vertex.s
@@ -8,8 +8,9 @@ attribute vec3 vertex_OS;
 attribute vec3 normal_OS;
 
 void main() {
-    gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * vec4(vertex_OS, 1);
+    vec4 pos = vec4(vertex_OS, 1);
+    gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * pos;
 
-    vert_light_dir = (gl_ModelViewMatrix * gl_Vertex).xyz;
+    vert_light_dir = (gl_ModelViewMatrix * pos).xyz;
     vert_norm_dir = gl_NormalMatrix * normal_OS;
 }
